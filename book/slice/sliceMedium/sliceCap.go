1276e200590af33ccc89da43b5a86e94909c52e8
EnochStar <1181591005@zust.edu.cn>
2021-01-20 11:13:05 +0800

    切片扩展

--- book/slice/sliceMedium/sliceCap.go
+++ book/slice/sliceMedium/sliceCap.go
@@ -4,8 +4,23 @@ import (
 	"fmt"
 )
 
-func main() {
+// cap重新分配规律
+func capRule() {
+	s := make([]int, 0, 1)
+	c := cap(s)
 
+	for i := 0; i < 50; i++ {
+		s = append(s, i)
+		if n := cap(s); n > c {
+			fmt.Printf("cap: %d -> %d\n", c, n)
+			c = n
+		}
+	}
+}
+
+//超出原 slice.cap 限制，就会重新分配底层数组，即便原数组并未填满。
+
+func capOut() {
 	data := [...]int{0, 1, 2, 3, 4, 10: 0}
 	s := data[:2:3]
 	fmt.Printf("s : %v\n", s)
@@ -13,8 +28,12 @@ func main() {
 	fmt.Printf("s cap : %v\n", cap(s))
 	// 一次 append 两个值，超出 s.cap 限制。
 	s = append(s, 100, 200)
-
+	fmt.Printf("s cap:%v\n", cap(s))
 	fmt.Println(s, data)         // 重新分配底层数组，与原数组无关。
 	fmt.Println(&s[0], &data[0]) // 比对底层数组起始指针。
+}
+func main() {
+
+	capRule()
 
 }
