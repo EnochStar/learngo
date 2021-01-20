23e815c9c17f5089d3b4481d39f3a2502194064e
EnochStar <1181591005@zust.edu.cn>
2021-01-20 19:37:02 +0800

    结构体定义与初始化

--- book/struct/structBase.go
+++ book/struct/structBase.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+	city string
+}
+
 func main() {
+	var p1 person
+	p1.name = "enoch"
+	p1.age = 10
+	p1.city = "WenZhou"
+
+	fmt.Printf("p1 = %v\n", p1)
+	fmt.Printf("p1 = %#v\n", p1)
 
 }
