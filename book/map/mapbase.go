cb4a97fbd7ae140c60863b57c3b244e6a31e71ca
EnochStar <1181591005@zust.edu.cn>
2021-01-20 15:37:40 +0800

    map的基本使用

---
+++ book/map/mapbase.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func assumeMap() {
+	scoreMap := make(map[string]int, 8)
+	scoreMap["小明"] = 10
+	scoreMap["张三"] = 19
+	fmt.Println(scoreMap)
+	fmt.Println(scoreMap["小明"])
+	fmt.Printf("type of a:%T\n", scoreMap)
+}
+func assumeMapAdvance() {
+	userInfo := map[string]string{
+		"username": "enoch",
+		"password": "zero",
+	}
+	fmt.Println(userInfo)
+}
+func main() {
+	assumeMapAdvance()
+}
