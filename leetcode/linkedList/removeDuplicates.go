e33f5d8f0b37e19e308c101648f6ce175238d8d2
EnochStar <1181591005@zust.edu.cn>
2021-01-28 19:56:32 +0800

    26. 删除排序数组中的重复项

---
+++ leetcode/linkedList/removeDuplicates.go
@@ -0,0 +1,17 @@
+package linkedList
+
+func removeDuplicates(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	fis := 0
+	las := 1
+	for las != len(nums) {
+		if nums[fis] != nums[las] {
+			fis++
+			nums[fis] = nums[las]
+		}
+		las++
+	}
+	return fis + 1
+}
