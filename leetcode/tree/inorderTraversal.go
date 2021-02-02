724dda1d775bf812e2da5e8abdaa72e9be79ad0d
EnochStar <1181591005@zust.edu.cn>
2021-02-02 10:13:53 +0800

    中序遍历树

---
+++ leetcode/tree/inorderTraversal.go
@@ -0,0 +1,14 @@
+package tree
+
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		inorder(root.Left)
+		res = append(res, root.Val)
+		inorder(root.Right)
+	}
+	return res
+}
