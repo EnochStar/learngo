38a39d5d3c62f6940bf14c293f68d3b8d4233393
EnochStar <1181591005@zust.edu.cn>
2021-02-05 15:58:32 +0800

    二叉树的最近公共祖先

---
+++ leetcode/tree/lowestCommonAncestor.go
@@ -0,0 +1,23 @@
+package tree
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	} else if left == nil && right == nil {
+		return nil
+	} else {
+		if left == nil {
+			return right
+		} else {
+			return left
+		}
+	}
+}
